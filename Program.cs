2883b14896c8b2f91369248dbd4079d9fc7a5b40
mdileep <m.dileep@gmail.com>
2015-05-06 01:41:20 +0530

    Initial stub
    Signed-off-by: mdileep <m.dileep@gmail.com>

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using Research.Services.Siri;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Siri
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.ForegroundColor = ConsoleColor.White;
+			Console.Title = "Siri Bhoovalaya-Stub";
+			DateTime st = DateTime.Now;
+			try
+			{
+				new TestSiri().Run();
+			
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+			finally
+			{
+				Console.Beep();
+			}
+			Console.WriteLine("----------------------------------");
+			Console.WriteLine("Time taken(in Min):  " + (DateTime.Now - st).TotalMinutes.ToString("0.00"));
+			Console.WriteLine("----------------DONE--------------");
+			Console.Write("Press any Key to Exit.....");
+			Console.ReadKey();
+		}
+	}
+}
